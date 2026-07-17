5f49f67c397a4284ab5865fff011dcd32824333b
Mohammed Kaif K <kk9127357@gmail.com>
2026-07-17 16:56:00 +0530

    Delete Foresight-crime-detection/Mohammad_CLG directory

--- Foresight-crime-detection/Mohammad_CLG/crime-prediction-platform/ml-service/inspect_lat.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df=pd.read_csv(r'c:\Users\gowth\Desktop\Mohammad_CLG\Final\Final\TamilNadu_FIR_Dataset_1Lakh_Claude.csv')
-zeros = (df['Latitude'].fillna(0) == 0).sum()
-with open("result.txt", "w") as f:
-    f.write(str(zeros))
